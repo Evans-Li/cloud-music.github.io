e8dd4ac242433f475712d7c3268c999018750a25
Axing <43921905+LXxxx1@users.noreply.github.com>
2020-04-03 19:06:57 +0800

    singerList 不能显示图片

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -7,33 +7,40 @@ import {
   List,
   ListItem
 } from './style'
+import {
+  getSingerList,
+  getHotSingerList,
+  changeEnterLoading,
+  changePageCount,
+  refreshMoreSingerList,
+  changePullUpLoading,
+  changePullDownLoading,
+  refreshMoreHotSingerList
+} from './store/actionCreators';
 import Scroll from '../../baseUI/Scroll';
-function Singers() {
-  const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17].map(item => {
-    return {
-      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
-      name: "隔壁老樊",
-      accountId: 277313426,
-    }
-  });
+import { connect } from 'react-redux'
 
-
+function Singers(props) {
+  const { singerList, enterLoading, pullUpLoading, pullDownLoading, pageCount } = props
+  const { updateDispatch, getHotSingerDispatch, pullUpRefreshDispatch, pullDownRefreshDispatch} = props
   const [category, setCategory] = useState('')
   const [alpha, setAlpha] = useState('')
-  let handleUpdateAlpha = (val) => {
+  const handleUpdateAlpha = (val) => {
     setAlpha(val)
+    updateDispatch(category, val)
   }
-  let handleUpdateCategory = (val) => {
+  const handleUpdateCategory = (val) => {
     setCategory(val)
+    updateDispatch(val, alpha)
   }
 
   const renderSingerList = () => {
     return (
       <List>
         {
-          singerList.map(item => {
+          singerList.map((item,index) => {
             return (
-              <ListItem>
+              <ListItem key={index}>
                 <div className='img_wrapper'>
                   <img src={`${item.picUrl}?param=300x300`} width='100%' height='100%' alt='music' />
                 </div>
@@ -45,6 +52,14 @@ function Singers() {
       </List>
     )
   }
+
+  useEffect(()=>{
+    getHotSingerDispatch()
+  },[])
+  useEffect(()=>{
+    console.log(props.singerList);
+  },[])
+
 
   return (
     <div>
@@ -68,8 +83,50 @@ function Singers() {
         </Scroll>
       </ListContainer>
     </div>
-
   )
+ }
+
+  const mapStateToProps = (state) => ({
+    singerList: state.getIn(['singers', 'singerList']),
+    enterLoading: state.getIn(['singers', 'enterLoading']),
+    pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+    pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+    pageCount: state.getIn(['singers', 'pageCount'])
+  })
+
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      getHotSingerDispatch() {
+        dispatch(getHotSingerList())
+      },
+      updateDispatch(category, alpha) {
+        dispatch(changePageCount(0))  //因为改变了分类, 所以count清零
+        dispatch(changeEnterLoading(false))
+        dispatch(getSingerList(category, alpha))
+
+      },
+      //h滑动到底部刷新
+      pullUpRefreshDispatch(category, alpha, hot, count) {
+        dispatch(changePullUpLoading(true));
+        dispatch(changePageCount(count + 1));
+        if (hot) {
+          dispatch(refreshMoreHotSingerList());
+        } else {
+          dispatch(refreshMoreSingerList(category, alpha));
+        }
+      },
+      //顶部下拉刷新
+      pullDownRefreshDispatch(category, alpha){
+        dispatch(changePullDownLoading(true))
+        dispatch(changePageCount(0))
+        if(category === '' && alpha === ''){
+          dispatch(getHotSingerList())
+        } else {
+          dispatch(getSingerList(category, alpha)) 
+        }
+      }
+    }
 }
 
-export default React.memo(Singers)
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Singers));
+
--- src/application/Singers/store/actionCreators.js
+++ src/application/Singers/store/actionCreators.js
@@ -7,6 +7,7 @@ import {
   CHANGE_PULLDOWN_LOADING}
 from './constast'
 import { fromJS } from 'immutable'
+// import { changeEnterLoadig } from '../../Recommend/store/actionCreators'
 
 export const changeSingerList = (data) =>({
   type: CHANGE_SINGER_LIST,
@@ -29,3 +30,62 @@ export const changePullDownLoading = (data) => ({
   type: CHANGE_PULLDOWN_LOADING,
   data
 });
+
+//第一次加载热门歌手
+export const getHotSingerList = () => {
+  return (dispatch) => {
+    getHotSingerListRequest(0).then((res) => {
+      const data = res.artists
+      dispatch(changeSingerList(data))
+      dispatch(changeEnterLoading(false))
+      dispatch(changePullDownLoading(false))
+    })
+    .catch(()=> {
+      console.log('获取热门歌手失败！');
+    })
+  }
+}
+
+// 加载更多热门歌手数据
+export const refreshMoreHotSingerList = () => {
+  return ((dispatch, getState)=>{
+    const pageCount = getState().getIn(['singers', 'pageCount'])
+    const singerList = getState().getIn(['singers', 'singerList']).toJS()
+    getHotSingerListRequest(pageCount).then((res)=>{
+      const data = [...singerList, ...res.artists]
+      dispatch(changeSingerList(data))
+      dispatch(changePullUpLoading(false))
+    }).catch(()=>{
+      console.log('获取热门歌手数据失败!');
+    })
+  })
+}
+
+// 第一次加载对应类别歌手
+export const getSingerList = ( category, alpha )=>{
+  return ((dispatch)=>{
+    getSingerListRequest(category, alpha, 0).then((res)=>{
+      let data = res.artists
+      dispatch(changeSingerList(data))
+      dispatch(changeEnterLoading(false))
+      dispatch(changePullDownLoading(false))
+    }).catch(()=>{
+      console.log('歌手列表数据获取失败');
+    })
+  })
+}
+
+//加载跟多歌手
+export const refreshMoreSingerList = (category, alpha)=>{
+  return((dispatch, getState)=>{
+    let pageCount = getState().getIn(['singers', 'pageCount'])
+    let singerList = getState().getIn(['singers', 'singerList'])
+    getSingerListRequest(category,alpha, pageCount).then((res)=>{
+      let data = [...singerList, ...res.artists]
+      dispatch(changeSingerList(data))
+      dispatch(changePullUpLoading(false))
+    }).catch(()=>{
+      console.log('更多歌手数据获取失败')
+    })
+  })
+}
