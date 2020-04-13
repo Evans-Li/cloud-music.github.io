5d312f169ce021fa95ba5cd2b963b4528b5821a3
Axing <43921905+LXxxx1@users.noreply.github.com>
2020-04-13 16:41:11 +0800

    recommend  进入歌单动画效果,

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group'
 import { Container } from './style'
 function Album(props){
-  const { route } = props
+
+  const [showStatus, setShowStatus] = useState (true);
+
   useEffect(()=>{
     console.log('album')
   },[])
   return(
+
+    <CSSTransition
+      in={showStatus}  
+      timeout={300} 
+      classNames="fly" 
+      appear={true} 
+      unmountOnExit
+      onExited={props.history.goBack}
+    >
       <Container>
-        dddd
+        sss
       </Container>
+    </CSSTransition>
   )
 }
 
